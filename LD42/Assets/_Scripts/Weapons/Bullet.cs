85c8250c50c8c1e7b556cf3cca4f5f1a9d51a530
Jakub Szatkowski <mistrzswiata.salata@gmail.com>
2018-08-12 15:10:02 +0200

    Taking Damages

--- LD42/Assets/_Scripts/Weapons/Bullet.cs
+++ LD42/Assets/_Scripts/Weapons/Bullet.cs
@@ -15,4 +15,11 @@ public class Bullet : MonoBehaviour
         m_Body.velocity = transform.up * stats.m_BulletSpeed;
     }
 
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.GetComponent<LivingEntity>().TakeDamage(100);
+        }
+    }
 }
