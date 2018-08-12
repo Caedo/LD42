bea7d4d70e8791e0e95b658eb7f89fe6505973fa
Mateusz Minus Wojtoń <theminusoid@gmail.com>
2018-08-12 20:37:47 +0200

    Working (?)  Main Menu
    Here we go!

---
+++ LD42/Assets/_Scripts/AudioScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioScript : MonoBehaviour
+{
+
+    public AudioMixer mixer;
+
+    public void SetMasterVolume(float level)
+    {
+        mixer.SetFloat("MasterVolume", (-80.0f * Mathf.Pow(Mathf.Abs(level), 6)));
+    }
+
+    public void SetMusicVolume(float level)
+    {
+        mixer.SetFloat("MusicVolume", (-80.0f * Mathf.Pow(Mathf.Abs(level), 6)));
+    }
+
+    public void SetEffetcsVolume(float level)
+    {
+        mixer.SetFloat("EffetcsVolume", (-80.0f * Mathf.Pow(Mathf.Abs(level), 6)));
+    }
+}
---
+++ LD42/Assets/_Scripts/LoadSceneScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneScript : MonoBehaviour
+{
+    public void LoadSceneByName(string SceneName)
+    {
+        Debug.Log(SceneName);
+        SceneManager.LoadScene(SceneName);
+    }
+}
