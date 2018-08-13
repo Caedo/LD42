d2f142f1919b473e17c839a8611b292d35d1172b
Mateusz Minus Wojtoń <theminusoid@gmail.com>
2018-08-13 19:48:13 +0200

    EnemyStarted

--- LD42/Assets/_Scripts/GameManager.cs
+++ LD42/Assets/_Scripts/GameManager.cs
@@ -7,14 +7,26 @@ public class GameManager : MonoBehaviour
 
     public Maze m_Maze;
     public PlayerController m_PlayerPrefab;
+    public GameObject m_Enemy;
+
+    Vector3 pos;
 
     private void Start()
     {
         m_Maze.GenerateMap();
         var startRoomTransform = m_Maze.StartRoom.transform;
+        pos = startRoomTransform.position;
         var player = Instantiate(m_PlayerPrefab, startRoomTransform.position, Quaternion.identity);
-        
+
+
         //JUST FOR NOW!
         Camera.main.GetComponent<SimpleCameraFollow>().m_Target = player.transform;
+
+        InvokeRepeating("SpawnEnemy", 10.0f, 0.5f);
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(m_Enemy, pos, Quaternion.identity);
     }
 }
